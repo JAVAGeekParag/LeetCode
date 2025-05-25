ec547d0ea5d5c627110bf9497d46cd5284651651
Parag Lalwani <61080527+JAVAGeekParag@users.noreply.github.com>
2025-05-25 16:06:39 +0530

    Time: 1 ms (98.46%), Space: 45 MB (31.92%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -1,18 +1,29 @@
 class Solution {
-    public void duplicateZeros(int[] arr) {
+      /*
+  O(n) runtime
+  O(1) space
+  */
+  public void duplicateZeros(int[] A) {
+    
+    int n = A.length, count = 0;
+    
+    for (int num : A) if (num == 0) count++;
+    int i = n - 1;
+    int write = n + count - 1;
+    
+    while (i >= 0 && write >= 0)  {
+      
+      if (A[i] != 0) { // Non-zero, just write it in
+        if (write < n) A[write] = A[i];
         
-        for(int i = 0;i<arr.length;i++)
-        {
-            if(arr[i] == 0)
-            {
-                int j =arr.length - 1;
-                for(;j>i;j--)
-                {
-                    arr[j] = arr[j-1];
-                }
-                i = j+1;
-            }
-        }
-        
+      } else { // Zero found, write it in twice if we can
+        if (write < n) A[write] = A[i];
+        write--;
+        if (write < n) A[write] = A[i];
+      }
+      
+      i--;
+      write--;
     }
+  }
 }
